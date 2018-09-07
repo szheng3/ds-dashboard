6145d7b989f84a46826e908dd90d79654a9f3ee8
Shuai <szheng3@uci.edu>
2018-09-07 15:47:24 -0700

    change the info

--- src/layouts/UserLayout.js
+++ src/layouts/UserLayout.js
@@ -25,7 +25,7 @@ const links = [
 
 const copyright = (
   <Fragment>
-    Copyright <Icon type="copyright" /> 2018 蚂蚁金服体验技术部出品
+    Copyright <Icon type="copyright"/> 2018 Digital Software
   </Fragment>
 );
 
@@ -34,9 +34,9 @@ class UserLayout extends React.PureComponent {
   // getPageTitle() {
   //   const { routerData, location } = this.props;
   //   const { pathname } = location;
-  //   let title = 'Ant Design Pro';
+  //   let title = 'Digital Software';
   //   if (routerData[pathname] && routerData[pathname].name) {
-  //     title = `${routerData[pathname].name} - Ant Design Pro`;
+  //     title = `${routerData[pathname].name} - Digital Software`;
   //   }
   //   return title;
   // }
@@ -51,10 +51,10 @@ class UserLayout extends React.PureComponent {
             <div className={styles.header}>
               <Link to="/">
                 <img alt="logo" className={styles.logo} src={logo} />
-                <span className={styles.title}>Ant Design</span>
+                <span className={styles.title}>Digital Software</span>
               </Link>
             </div>
-            <div className={styles.desc}>Ant Design 是西湖区最具影响力的 Web 设计规范</div>
+            <div className={styles.desc}>Digital Software 记账内部测试系统</div>
           </div>
           {children}
         </div>
